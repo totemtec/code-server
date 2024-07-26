a859c0f251e517a1b16611c9f5066c9d7ec64aca
马江林 <majianglin@totemtec.com>
2024-07-26 13:34:27 +0800

    update ssl config

--- src/node/cli.ts
+++ src/node/cli.ts
@@ -637,9 +637,10 @@ export function getResolvedPathsFromArgs(args: UserProvidedArgs): string[] {
  */
 export function defaultConfigFile(password: string): string {
   return `bind-addr: 127.0.0.1:8080
-auth: password
-password: ${password}
-cert: false
+auth: none
+password: none
+cert: /etc/ssl/canhoo.vip_chain.crt
+cert-key: /etc/ssl/canhoo.vip_public.crt
 `
 }
 
